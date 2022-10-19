c7aa07a198460445a49996de9c73d26439b6be62
unknown <Savioanjos2020@gmail.com>
2022-10-19 20:39:58 -0300

    Previnindo erros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ import { router } from './routes';
   app.use(express.json());
   app.use(cors());
 
-  app.use(router);
+  app.use(router); 
 
   app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
       if(err instanceof Error) {
