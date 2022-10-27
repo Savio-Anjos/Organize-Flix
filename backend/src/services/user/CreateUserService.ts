bbda1922dd5169689cbc9ffb14a3385cf2f564ae
unknown <Savioanjos2020@gmail.com>
2022-10-27 09:10:02 -0300

    Criando funçao para receber as informações do usuario

---
+++ backend/src/services/user/CreateUserService.ts
@@ -0,0 +1,15 @@
+
+  interface UserRequest {
+    name: string;
+    email: string;
+    password: string;
+  }
+
+  class CreateUserService {
+    async execute({name, email, password}: UserRequest) {
+       
+        return {name: name }
+    }
+  }
+
+  export { CreateUserService }
