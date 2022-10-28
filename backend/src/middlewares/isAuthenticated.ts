fcfc9e5de7deba76289e8b6047971b7a620702dd
unknown <Savioanjos2020@gmail.com>
2022-10-28 10:24:10 -0300

    Validando usuário ayravés do middleware

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -1,11 +1,36 @@
 import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+  interface Payload {
+    sub: string;
+  }
 
   export function isAuthenticated(
     req: Request,
     res: Response,
     next: NextFunction
   ) {
-    console.log("CHAMOU ESSE MIDDLEWARE")
+    //Receber o token
+    const authToken = req.headers.authorization;
 
-    return next();
+    if(!authToken) {
+        return res.status(401).end();
+    }
+
+    const [, token] = authToken.split(" ")
+
+    try {
+      //validar esse token
+      const { sub } = verify(
+        token,
+        process.env.JWT_SECRET
+      ) as Payload
+
+      return next();
+      
+    } catch (err) {
+      return res.status(401).end();
+    }
   }
+
+  
