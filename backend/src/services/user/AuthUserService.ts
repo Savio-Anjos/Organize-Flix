0c6b142d1854b902dc109d27de737a9ebe26abcc
unknown <Savioanjos2020@gmail.com>
2022-10-28 07:03:59 -0300

    Configurando login com token JWT

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,6 +1,7 @@
 
   import prismaClient from "../../prisma";
-  import { compare } from 'bcryptjs'
+  import { compare } from 'bcryptjs';
+  import { sign } from 'jsonwebtoken';
   
   interface AuthRequest {
     email: string;
@@ -27,11 +28,27 @@
             throw new Error("User/password incorrect")
         }
 
-        // gerar um token JWT e devolver os dados do usuário como id, name e email
+        // Se deu tudo certo vamos gerar um token para o usuário.
+        const token = sign (
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET, 
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
 
 
 
-        return { ok: true }
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
 
     }
   }
