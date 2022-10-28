babffcdec4a397cffdd57c9656811df5d17aee07
unknown <Savioanjos2020@gmail.com>
2022-10-28 11:59:16 -0300

    Finalizando sisema de login

--- backend/src/controllers/user/DetailUserController.ts
+++ backend/src/controllers/user/DetailUserController.ts
@@ -4,9 +4,11 @@ import { DetailUserService } from "../../services/user/DetailUserService";
    class DetailUserController {
     async handle(req: Request, res: Response) {
 
+        const user_id  = req.user_id;
+
         const detailUserService = new DetailUserService();
 
-        const user = await detailUserService.execute();
+        const user = await detailUserService.execute(user_id);
 
         return res.json(user);
 
