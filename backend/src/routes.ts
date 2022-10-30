c6613336e8f023120d134bb3533bb3bb4c7175ce
unknown <Savioanjos2020@gmail.com>
2022-10-29 23:59:19 -0300

    Finalizando sistema de cadastro de categoria

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -14,5 +15,10 @@ import { isAuthenticated } from "./middlewares/isAuthenticated";
   router.post('/session', new AuthUserController().handle)
 
   router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+
+  //-- ROTAS CATEGORY --
+
+  router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
   export { router };
