1f1e4fc6bec42ff53d636e1c18731f666510a93a
unknown <Savioanjos2020@gmail.com>
2022-11-28 09:06:01 -0300

    Criando página de cadastro

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,33 +1,40 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import styles from '../../styles/home.module.scss';
+import styles from '../../../styles/home.module.scss';
 
-import logoImg from '../../public/logo.svg';
+import logoImg from '../../../public/logo.svg';
 
 //COMPONENTS
-import { Input } from "../components/ui/Input";  
-import { Button } from "../components/ui/Button";
+import { Input } from "../../components/ui/Input";  
+import { Button } from "../../components/ui/Button";
 
 import Link from 'next/link';
 
 
 
 
-export default function Home() {
+export default function SignUp() {
   return (
    <>
    <Head>
-     <title>OrganizeFlix - Faça  seu login</title>
+     <title>Faça seu cadastro agora!</title>
    </Head>
    
    <div className={styles.containerCenter}>
      <Image src={logoImg} alt="Logo Organize Flix"/>
      
-     <h3 className={styles.subTitle}>Faça login e organize seus filmes!</h3>
+     <h3 className={styles.subTitle}>Cadastre-se e organize seus filmes!</h3>
 
      <div className={styles.login}>
        <form>
+
+       <h4 className={styles.label}>Nome de usuário:</h4>
+
+       <Input
+         placeholder="Digite seu nome"
+         type="text"
+        />
 
        <h4 className={styles.label}>Endereço de email:</h4>
 
@@ -46,14 +53,14 @@ export default function Home() {
          type="submit"
          loading={false}
         >
-          Acessar
+          Cadastrar
 
         </Button>
 
        </form>
 
-       <Link href="/signup" legacyBehavior>
-         <a className={styles.text}>Não possui uma conta? Cadastre-se!</a>
+       <Link href="/" legacyBehavior>
+         <a className={styles.text}>Já possui uma conta? Faça login!</a>
        </Link>
      </div>
    </div>
