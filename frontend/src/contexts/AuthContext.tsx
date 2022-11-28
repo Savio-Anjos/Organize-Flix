e8c0bbb262bede63fc1a19147b6cc96f1e4ca736
unknown <Savioanjos2020@gmail.com>
2022-11-28 11:44:59 -0300

    Criando função para deslogar o usuário

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,9 +1,13 @@
 import { createContext, ReactNode, useState } from 'react';
+import Router from 'next/router';
+
+import { destroyCookie } from 'nookies';
 
 type AuthContextData = {
     user: UserProps;
     isAuthenticated: boolean;
     signIn: (credentials: SignInProps) => Promise<void>;
+    signOut: () => void;
 }
 
 type UserProps = {
@@ -23,6 +27,15 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut() {
+    try {
+        destroyCookie(undefined, '@nextauth.token')
+        Router.push('/')
+    } catch {
+        console.log('Erro ao deslogar')
+    }
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<UserProps>()
     const isAuthenticated = !!user;
@@ -33,7 +46,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
     }
 
     return (
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
            {children}
         </AuthContext.Provider>
     )
