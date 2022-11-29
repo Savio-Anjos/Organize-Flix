db2b8c65c9f0a6bdf11818ec260131bb79427b4d
unknown <Savioanjos2020@gmail.com>
2022-11-29 20:42:14 -0300

    Iniciando sistema de cadastro

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from '../../../styles/home.module.scss';
@@ -15,6 +15,23 @@ import Link from 'next/link';
 
 
 export default function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleSignup(event: FormEvent) {
+    event.preventDefault();
+
+    if(name === '' || email === '' || password === '') {
+      alert("PREENCHA TODOS OS CAMPOS")
+      return;
+    } 
+
+    setLoading(true);
+  }
+
   return (
    <>
    <Head>
@@ -27,13 +44,15 @@ export default function SignUp() {
      <h3 className={styles.subTitle}>Cadastre-se e organize seus filmes!</h3>
 
      <div className={styles.login}>
-       <form>
+       <form onSubmit={handleSignup}>
 
        <h4 className={styles.label}>Nome de usuário:</h4>
 
        <Input
          placeholder="Digite seu nome"
          type="text"
+         value={name}
+         onChange={ (e) => setName(e.target.value)}
         />
 
        <h4 className={styles.label}>Endereço de email:</h4>
@@ -41,17 +60,21 @@ export default function SignUp() {
         <Input
          placeholder="joao@exemple.com"
          type="text"
+         value={email}
+         onChange={ (e) => setEmail(e.target.value)}
         />
 
        <h4 className={styles.label}>Sua senha:</h4>
 
         <Input placeholder="********"
         type="password"
+        value={password}
+        onChange={ (e) => setPassword(e.target.value)}
         />
 
         <Button
          type="submit"
-         loading={false}
+         loading={loading}
         >
           Cadastrar
 
