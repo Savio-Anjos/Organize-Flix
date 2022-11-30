fe102596af48505581f51560e02d034521e02e7a
unknown <Savioanjos2020@gmail.com>
2022-11-29 21:21:04 -0300

    Finalizando sistema de cadastro

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -9,6 +9,7 @@ type AuthContextData = {
     isAuthenticated: boolean;
     signIn: (credentials: SignInProps) => Promise<void>;
     signOut: () => void;
+    signUp: (credentials: SignUpProps) => Promise<void>;
 }
 
 type UserProps = {
@@ -18,6 +19,12 @@ type UserProps = {
 }
 
 type SignInProps = {
+    email: string;
+    password: string;
+}
+
+type SignUpProps = {
+    name: string;
     email: string;
     password: string;
 }
@@ -74,8 +81,27 @@ export function AuthProvider({ children }: AuthProviderProps) {
         }
     }
 
+    async function signUp({name, email, password }:SignUpProps) {
+        try {
+            
+            const response = await api.post('/users', {
+                name,
+                email,
+                password
+            })
+
+            console.log("Cadastrado com sucesso!")
+
+            Router.push('/')
+
+
+        } catch (err) {
+            console.log("Erro ao cadastrar ", err)
+        }
+    }
+
     return (
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut, signUp }}>
            {children}
         </AuthContext.Provider>
     )
--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useContext } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from '../../../styles/home.module.scss';
@@ -9,12 +9,17 @@ import logoImg from '../../../public/logo.svg';
 import { Input } from "../../components/ui/Input";  
 import { Button } from "../../components/ui/Button";
 
+import { AuthContext } from "../../contexts/AuthContext";
+
 import Link from 'next/link';
 
 
 
 
 export default function SignUp() {
+
+  const { signUp } = useContext(AuthContext);
+
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -30,6 +35,16 @@ export default function SignUp() {
     } 
 
     setLoading(true);
+
+    let data = {
+      name,
+      email,
+      password
+    }
+
+    await signUp(data)
+
+    setLoading(false)
   }
 
   return (
