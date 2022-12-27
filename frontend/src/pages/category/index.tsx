0ce5a3f7ad28068c395b2d499e14a694815ef54c
unknown <Savioanjos2020@gmail.com>
2022-12-27 07:28:26 -0300

    Finalizando funcionalidade para cadastrar categoria

--- frontend/src/pages/category/index.tsx
+++ frontend/src/pages/category/index.tsx
@@ -1,9 +1,33 @@
+import { FormEvent, useState } from "react";
 import Head from "next/head";
 import { Header } from "../../components/Header"; 
-import styles from "./style.module.scss"
+import styles from "./style.module.scss";
+
+import { setupApiClient } from "../../services/api";
+import { toast } from "react-toastify";
 
 
  export default function Category() {
+    const [name, setName] = useState('');
+
+    async function handleRegister(event: FormEvent) {
+        event.preventDefault();
+
+        if(name === '') {
+            toast.warning('Prencha o campo antes de cadastrar!')
+            return;
+        }
+
+        const apiClient = setupApiClient();
+        await apiClient.post('/category', {
+            name: name
+        })
+
+        toast.success('Categoria cadastrada com sucesso!')
+        setName('');
+
+    }
+
     return (
         <>
          <Head>
@@ -16,10 +40,12 @@ import styles from "./style.module.scss"
             <main className={styles.container}>
                 <h1>Cadastrar Categoria</h1>
 
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleRegister}>
                     <input type="text"
                      placeholder="Digite uma nova Categoria" 
                      className={styles.input}
+                     value={name}
+                     onChange={(e) => setName(e.target.value)}
                      />
 
                     <button className={styles.buttonAdd} type="submit">
